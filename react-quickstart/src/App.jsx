093e0629069958af46f4b124e29283490d4c5f28
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-09-26 16:55:17 +0800

    Save local changes before pulling

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,13 +1,12 @@
-// App.jsx
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Welcome />
+      <h1>Sharing Data</h1>
+      <Child message="Hello from parent!" />
     </div>
   );
 }
 
-function Welcome() {
-  return <h2>Hello, I am Juan Miguel Ramirez</h2>;
+function Child({ message }) {
+  return <p>{message}</p>;
 }

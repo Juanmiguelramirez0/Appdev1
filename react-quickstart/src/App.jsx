f60608c24c5294cd458a8e9f99e3e622949ce750
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-09-26 16:12:10 +0800

    My personal portfolio template for the midterm project

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+function Welcome() {
+  return <h2>Hello, I am Juan Miguel Ramirez</h2>;
+}

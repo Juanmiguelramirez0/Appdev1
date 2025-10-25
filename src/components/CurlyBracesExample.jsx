5c705269b7e69983c595c4a622a2ff7dd8be95a6
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-10-25 20:40:16 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

---
+++ src/components/CurlyBracesExample.jsx
@@ -0,0 +1,6 @@
+export default function CurlyBracesExample() {
+  const name = "React Student";
+  const year = new Date().getFullYear();
+
+  return <p>Hello {name}, the current year is {year}.</p>;
+}

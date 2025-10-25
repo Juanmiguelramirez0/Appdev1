0e602408398f6eac9c19f94ce2686868df5ed1c0
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-10-25 20:54:10 +0800

    Save before rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import FirstComponent from "./components/FirstComponent";
+import MultiComponent from "./components/MultiComponent";
+import MarkupExample from "./components/MarkupExample";
+import CurlyBracesExample from "./components/CurlyBracesExample";
+import PropsExample from "./components/PropsExample";
+import ConditionalExample from "./components/ConditionalExample";
+import MultipleRenderExample from "./components/MultipleRenderExample";
+import PureComponentExample from "./components/PureComponentExample";
+import UITreeExample from "./components/UITreeExample";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <FirstComponent />
+      <MultiComponent />
+      <MarkupExample />
+      <CurlyBracesExample />
+      <PropsExample />
+      <ConditionalExample />
+      <MultipleRenderExample />
+      <PureComponentExample value={5} />
+      <UITreeExample />
+    </div>
+  );
 }
-
-export default App

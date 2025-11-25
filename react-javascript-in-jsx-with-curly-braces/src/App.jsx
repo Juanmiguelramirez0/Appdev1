5a08ce3dd7b317dddeb1b1ddf6291ca36c4d855b
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-11-25 19:16:08 +0800

    Done with using double curlies: CSS and other objects in JSX

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Avatar from "./component/Avatar";
+import ToDoList from "./component/TodoList";
+
+function App() {
+  return (
+    <>
+      <Avatar />
+      <ToDoList />
+
+    </>
+  );
+}
+
+export default App;
+

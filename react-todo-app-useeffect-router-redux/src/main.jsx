1376a812c53ec0c325bd87c33afe79fe8e05f2e2
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-11-28 21:23:31 +0800

    Done with connect Redux to the app section

--- react-todo-app-useeffect-router-redux/src/main.jsx
+++ react-todo-app-useeffect-router-redux/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="todos" element={<Todos />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
